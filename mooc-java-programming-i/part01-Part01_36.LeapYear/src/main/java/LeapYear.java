9d79970aee484dc47ef7b33183a72725b458de49
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-10 17:17:48 +0800

    done with part 1!

--- mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -6,5 +6,17 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int year = Integer.valueOf(scan.nextLine());
+
+        String isLeap = "not a leap year.";
+
+        if (year % 4 == 0) {
+            isLeap = "a leap year.";
+            if (year % 100 == 0 && year % 400 != 0) {
+                isLeap = "not a leap year.";
+            }
+        }
+
+        System.out.println("The year is " + isLeap);
     }
 }
--- mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -6,5 +6,44 @@ public class GiftTax {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int giftVal = Integer.valueOf(scan.nextLine());
+
+        int rate = 0;
+        int base = 0;
+        int limit = 0;
+
+        if (giftVal > 1000000) {
+            base = 142100;
+            rate = 17;
+            limit = 1000000;
+        } else if (giftVal >= 200000) {
+            base = 22100;
+            rate = 15;
+            limit = 200000;
+        } else if (giftVal >= 55000) {
+            base = 4700;
+            rate = 12;
+            limit = 55000;
+        } else if (giftVal >= 25000) {
+            base = 1700;
+            rate = 10;
+            limit = 25000;
+        } else if (giftVal >= 5000) {
+            base = 100;
+            rate = 8;
+            limit = 5000;
+        }
+
+        int excess = giftVal - limit;
+
+        double processedRate = (1.0 * rate) / 100;
+
+        double tax = base + (excess * processedRate);
+
+        if (giftVal < 5000) {
+            System.out.println("No tax!");
+        } else {
+            System.out.println("Tax: " + tax);
+        }
     }
 }
